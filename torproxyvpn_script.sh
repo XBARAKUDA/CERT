81b8a0c7acd4c3fccbcca02fcae242dd5e68cbe8
X <131550891+XBARAKUDA@users.noreply.github.com>
2023-06-02 17:13:51 +0100

    Update torproxyvpn_script.sh

--- torproxyvpn_script.sh
+++ torproxyvpn_script.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
 
-# Fetch the public IP address using curl and store it in a variable
-public_ip=$(curl -s ifconfig.me)
-
-# Update the torrc file with the public IP address
-cat << EOF | sudo tee -a /etc/tor/torrc
-HTTPProxy $public_ip:443
-HTTPSProxy $public_ip:443
-EOF
-
-# Open the torrc file in nano editor for verification and further modifications if needed
-sudo nano /etc/tor/torrc
